f45acab6b05b5deb795a71790508bb6046a6a6da
khiemnv <khiemnv.work@gmail.com>
2025-04-06 10:36:58 +0700

    update

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // design file
-app.use(express.static('public'));
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
+app.use(express.static(__dirname + 'public'));
 // routers
 app.get('/', (req, res) => {
   res.render('index');
